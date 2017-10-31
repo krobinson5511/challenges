ff316327ffb03506aa1ffe932b393673a1db7174
krobinson5511 <33257689+krobinson5511@users.noreply.github.com>
2017-10-31 14:14:04 +0000

    Create ch2.java

---
+++ ch2.java
@@ -0,0 +1,26 @@
+package ch2;
+import java.util.Scanner;
+
+
+public class ch2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String country;
+        System.out.println("what is the name of the country");
+        country = sc.next();
+
+        String  population;
+        System.out.println("Enter the  population of the country that you have choosen");
+        population = sc.next();
+             
+             
+
+    
+    
+    
+    
+    System.out.println(country+"has a population of "+ population);
+    }
+}
